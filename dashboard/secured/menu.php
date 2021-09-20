c3ad3658743341e31cb2389de2bdf5746acb6202
Pedro Weinzettel <weinzettel.pedro@gmail.com>
2021-09-20 00:20:35 -0300

    menu corrected

--- dashboard/secured/menu.php
+++ dashboard/secured/menu.php
@@ -4,12 +4,6 @@ $active = basename($_SERVER['SCRIPT_NAME'], '.php');
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="/">XLX</a>
-
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-
-
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
