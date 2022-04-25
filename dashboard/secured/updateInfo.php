95856a16fd7dc0c5e65e44932ec7a281e97cc963
Pedro Weinzettel <weinzettel.pedro@gmail.com>
2022-04-24 21:35:19 -0300

    Update updateInfo.php

--- dashboard/secured/updateInfo.php
+++ dashboard/secured/updateInfo.php
@@ -8,7 +8,12 @@ $ReflectorHash = get_opt('ReflectorHash');
 $DashboardURL = get_opt('DashboardURL');
 $RefCountry = urlencode(get_opt('RefCountry'));
 $RefComment = urlencode(get_opt('RefComment'));
+
 $OverrideIP = get_opt('OverrideIP');
+
+if (!$OverrideIP) {
+   $OverrideIP = file_get_contents("http://ipecho.net/plain");
+}
 
 $file_handle = @fopen($ServerURL."?ReflectorName=".$ReflectorName."&ReflectorUptime=".$ServiceUptime."&ReflectorHash=".$ReflectorHash."&DashboardURL=".$DashboardURL."&Country=".$RefCountry."&Comment=".$RefComment."&OverrideIP=".$OverrideIP, "r");
 
